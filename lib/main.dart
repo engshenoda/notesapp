593d332540d882da3dfaa2b5ac40b9bce315143c
shenoda <engshenoda2020@gmail.com>
2024-10-08 17:07:33 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:notesapp/pages/home_page.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const HomePage(
+      
+    );
+  }
+}
