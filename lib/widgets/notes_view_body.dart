15992e2ff69428c59effbdb6ece0db2d3425781d
shenoda <engshenoda2020@gmail.com>
2024-10-13 19:48:31 +0300

    create notes list view

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notesapp/widgets/custom_app_bar.dart';
-import 'package:notesapp/widgets/custom_note_item.dart';
+import 'package:notesapp/widgets/notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -12,7 +12,12 @@ class NotesViewBody extends StatelessWidget {
       child: Column(
         children: [
           CustomAppBar(),
-          NoteItem(),
+          SizedBox(
+            height: 40,
+          ),
+          Expanded(
+            child: NotesListView(),
+          ),
         ],
       ),
     );
