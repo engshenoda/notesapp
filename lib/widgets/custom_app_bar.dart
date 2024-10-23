747ec54f1bd81cbb14afe7ed4f0b53d25a56c6b2
shenoda <engshenoda2020@gmail.com>
2024-10-23 23:08:36 +0300

    add packges hive

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key, required this.icon, required this.onPressed, required this.title});
+  const CustomAppBar(
+      {super.key,
+      required this.icon,
+      required this.onPressed,
+      required this.title});
   final IconData icon;
   final String title;
   final void Function()? onPressed;
@@ -10,7 +14,7 @@ class CustomAppBar extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-         Text(
+        Text(
           title,
           style: const TextStyle(fontSize: 30, fontWeight: FontWeight.w900),
         ),
