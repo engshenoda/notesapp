2bc52e652a5cec2ce125183cda78e660ae327b5e
shenoda <engshenoda2020@gmail.com>
2024-10-25 14:54:59 +0300

    register adapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notesapp/constans/colors.dart';
+import 'package:notesapp/models/note_model.dart';
 import 'package:notesapp/views/notes_view.dart';
 import 'package:notesapp/views/edit_note_view.dart';
 
 void main() async{
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
-  runApp(
-    const NotesApp()
-  );
+  Hive.registerAdapter(NoteModelAdapter());
+  runApp(const NotesApp());
 }
 
 class NotesApp extends StatelessWidget {
